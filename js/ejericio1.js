c31b829add426ef90558c0718f2c46eacca15649
¨Gerson¨ <gh0905869@gmail.com>
2025-01-15 18:18:32 -0500

    Primer commit

---
+++ js/ejericio1.js
@@ -0,0 +1,13 @@
+// 1. Dado un array de números [10, 20, 30, 40, 50, 60], crea un programa que calcule e imprima 
+// el promedio de los números en el array. 
+
+const numeros = [10, 20, 30, 40, 50, 60];
+
+let suma = 0;
+for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[1];
+}
+
+const promedio = suma / numeros.length;
+
+console.log("El promedio es:", promedio);
